4e855d59dcb1ed2b55280071107ff3a45a587d9a
Amr Essam <amr.essam@emirates.com>
2018-09-04 22:26:54 +0400

    Commented the code to help explain the changes

--- Game.js
+++ Game.js
@@ -19,15 +19,15 @@ export default class Game {
     }
 
     let numNeighbors = 0
-    let startRow = row - 1 < 0 ? 0 : row - 1
-    let endRow = row + 1 >= this.numRows ? this.numRows - 1 : row + 1
-    let startCol = col - 1 < 0 ? 0 : col - 1
-    let endCol = col + 1 >= this.numCols ? this.numCols - 1 : col + 1
+    let startRow = row - 1 < 0 ? 0 : row - 1 // the row before the cell row or the first row
+    let endRow = row + 1 >= this.numRows ? this.numRows - 1 : row + 1 // the row after the cell row or the last row
+    let startCol = col - 1 < 0 ? 0 : col - 1 // the col before the cell col or the first col
+    let endCol = col + 1 >= this.numCols ? this.numCols - 1 : col + 1 // the col after the cell col or the last col
 
-    for (let i = startRow; i <= endRow; i++) {
+    for (let i = startRow; i <= endRow; i++) { // loop through the cell neigbors including the current cell position
       for (let j = startCol; j <= endCol; j++) {
-        if (!(i === row && j === col)) {
-          numNeighbors += stateValues[this.state[i][j].state]
+        if (!(i === row && j === col)) { // ignore the current cell
+          numNeighbors += stateValues[this.state[i][j].state] // increment the numNeighbors variable ie total number of adjacent neighbors
         }
       }
     }
